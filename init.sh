7a351c6e9cbf56d80e9b2428d437992067d3698a
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2023-04-23 17:36:43 +0530

    transient
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- init.sh
+++ init.sh
@@ -2,14 +2,9 @@
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update
-sudo apt install -y build-essential stow git fzf ripgrep sqlite3 fd-find
+sudo apt install -y stow fzf ripgrep sqlite3 fd-find
 
 rm -rf ~/.zshrc ~/.zprofile ~/.zlogin ~/.zlogout
 mkdir ~/bin
-
-git clone https://github.com/utkarsh-pro/dotfiles.git /tmp/dotfiles
-pushd /tmp/dotfiles
-make setup
-popd
 
 curl -fsSL https://starship.rs/install.sh | sh -s -- -y
